526bdb3569bd8c4ae71cca2dc8df35a5af02b04e
Ethan Steigerwald <ethan.steigerwald@Mac.ht.home>
2025-08-12 23:22:16 -0400

    Fix JSX structure - remove extra closing div tags causing build error

--- src/App.tsx
+++ src/App.tsx
@@ -2307,7 +2307,6 @@ function AppContent() {
                       ))}
                     </div>
                   </div>
-                  </div>
                 </div>
               </div>
             )}
